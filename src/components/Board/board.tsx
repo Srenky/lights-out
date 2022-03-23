7aebcb537c4c7ee5480a10a20c78b617a2efd720
Srenky <srenky@email.com>
2022-03-23 18:06:30 +0100

    moved functions to helpers.ts, added solution button

--- src/components/Board/board.tsx
+++ src/components/Board/board.tsx
@@ -1,53 +1,15 @@
 import React from "react";
 import { Light } from "../Light/light";
-import { generateCells } from "../../utils/helpers"
-import { Cell, CellState } from "../../utils/types";
+import { generateCells, toggleLight, toggleAdjacentLights, isSolved } from "../../utils/helpers"
+import { Cell } from "../../utils/types";
 import './board.css'
 
 export const Board: React.FC = () => {
 
     const [size, setSize] = React.useState<number>(3);
     const [board, setBoard] = React.useState<Cell[][]>(generateCells(size, size));
-    const [moves, setMoves] = React.useState<number>(0);
+    const [moves, setMoves] = React.useState<number>(1);
     const [won, setWon] = React.useState<boolean>(false);
-
-    const toggleLight = (cell: Cell): void => {
-        if (cell.state === CellState.off) {
-            cell.state = CellState.on;
-        } else {
-            cell.state = CellState.off;
-        }
-    }
-
-    const toggleAdjacentLights = (b: Cell[][], row: number, col: number): void => {
-        // down
-        if (row > 0) {
-            toggleLight(b[row-1][col]);
-        }
-        // up
-        if (row < 2) {
-            toggleLight(b[row+1][col]);
-        }
-        // left
-        if (col > 0) {
-            toggleLight(b[row][col-1]);
-        }
-        // right
-        if (col < 2) {
-            toggleLight(b[row][col+1]);
-        }
-    }
-    
-    const isSolved = (): boolean => {
-        for (let row = 0; row < size; row++) {
-            for (let col = 0; col < size; col++) {
-                if (board[row][col].state === CellState.on) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 
     const handleLightClick = (row: number, col: number) => (): void => {
         if (won) return;
@@ -60,16 +22,21 @@ export const Board: React.FC = () => {
         setBoard(newBoard);
         setMoves(moves + 1);
 
-        if (isSolved()) {
+        if (isSolved(board)) {
             alert("Congratulations, you've won in " + moves +  " moves!");
             setWon(true);
         }
     }
 
     const handleGenerateClick = () => (): void => {
-        setMoves(0);
+        setMoves(1);
         setWon(false);
         setBoard(generateCells(size, size));
+    }
+
+    // TODO
+    const handleSolutionClick = () => (): void => {
+        
     }
 
     const renderCells = (): React.ReactNode => {
@@ -89,6 +56,7 @@ export const Board: React.FC = () => {
 
     return (<>
         <div className="Body">
+
             <div className="Header">
                 <label htmlFor="size">Choose size: </label>
                 <select name="size" id="size" value={size} onChange={(e) => setSize(Number(e.target.value))}>
@@ -98,10 +66,16 @@ export const Board: React.FC = () => {
                     <option value="6">6x6</option>
                 </select>    
             </div>
-            <button className="generateButton" onClick={handleGenerateClick()}>generate</button>
+
+            <div>
+                <button className="actionButton" onClick={handleSolutionClick()}>Solution</button>
+                <button className="actionButton" onClick={handleGenerateClick()}>Generate</button>
+            </div>
+            
             <div className="Board">
                 {renderCells()}
             </div>
+
         </div>
     </>)
 }
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,19 +1,64 @@
 import { Cell, CellState } from "./types";
 
+export const toggleLight = (cell: Cell): void => {
+    if (cell.state === CellState.off) {
+        cell.state = CellState.on;
+    } else {
+        cell.state = CellState.off;
+    }
+}
+
+export const toggleAdjacentLights = (b: Cell[][], row: number, col: number): void => {
+    // down
+    if (row > 0) {
+        toggleLight(b[row-1][col]);
+    }
+    // up
+    if (row < b.length-1) {
+        toggleLight(b[row+1][col]);
+    }
+    // left
+    if (col > 0) {
+        toggleLight(b[row][col-1]);
+    }
+    // right
+    if (col < b.length-1) {
+        toggleLight(b[row][col+1]);
+    }
+}
 
 export const generateCells = (rows: number, cols: number): Cell[][] => {
     let cells: Cell[][] = [];
 
+    // create light cells
     for (let row = 0; row < rows; row++) {
         cells.push([]);
         for (let col = 0; col < cols; col++) {
             cells[row].push({
-                state: CellState.off
+                state: CellState.off,
             })
         }
     }
 
-    
+    // turn some of them on
+    for (let i = 0; i < rows*cols; i++) {
+        var row = Math.floor(Math.random() * rows);
+        var col = Math.floor(Math.random() * cols);
+
+        toggleLight(cells[row][col]);
+        toggleAdjacentLights(cells, row, col);
+    }
 
     return cells;
+}
+
+export const isSolved = (cells: Cell[][]): boolean => {
+    for (let row = 0; row < cells.length; row++) {
+        for (let col = 0; col < cells.length; col++) {
+            if (cells[row][col].state === CellState.on) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
